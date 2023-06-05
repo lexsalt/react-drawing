20a99b6958c1dfd3618dc5bfde6b35c670992c60
emilianosal <emilianosal@hotmail.com>
2023-06-05 13:58:21 +0200

    cleaned code to start again

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 //import { UseRef } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -11,10 +11,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      width: 50,
+      height: 100,
+      color: "red",
       painting: true
     };
     this.handleClick = this.handleClick.bind(this);
   }
+
   handleClick() {
     const quotes = [
       ["Believe you can and you're halfway there.", "Theodore Roosevelt"],
@@ -30,45 +34,26 @@ class App extends React.Component {
       color: newColor,
     }));
   }
+   updateDimensions = () => {
+     this.setState({ width: window.innerWidth, height: window.innerHeight });
+   }
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+  
   render() {
-    //
-    //
-    //   const ref = useRef(null);
-
-    // const [width, setWidth] = useState(0);
-    // const [height, setHeight] = useState(0);
-
-    // useLayoutEffect(() => {
-    //   setWidth(ref.current.offsetWidth);
-    //   setHeight(ref.current.offsetHeight);
-    // }, []);
-
-    //
-    // let str1 = "https://twitter.com/intent/tweet?text="
-    // let link = str1.concat(this.state.quote+" ("+this.state.author+")")
-
+    const canvasWidth = this.state.width
+    const canvasHeight = this.state.height
+    const backgroundColor = this.state.color
     return (
-      //   <div id="bg" style={{background: this.state.color}}>
-      //   <div id="wrapper">
-      //       <div id="quote-box">
-      //          <div id='text' style={{color: this.state.color}}><h1>{this.state.quote}</h1></div>
-      //         <div id='author' style={{color: this.state.color}}><p>-</p><p>{this.state.author}</p></div>
-      //         <div id='buttons'>
-      //         <button id='new-quote' onClick={() => this.handleClick()} style={{background: this.state.color}}>New Quote</button>
-      //           <a class="twitter-share-button"
-      //   href={link} data-size="large" id='tweet-quote' style={tweetQuoteStyle}>
-      // Tweet this quote!</a>
-      //         </div>
-      //       </div>
-      // </div>
-      // </div>
       <div className="parent">
         <div id="sister" className="sister">
-          <Canvas
-            draw={(canvas, ctx) => {
-              ctx.strokeRect(5, 5, 5, 5);
-            }}
-          />
+        <canvas style={{ background: backgroundColor, outline: "13px solid pink", width:{canvasWidth},
+        height:{canvasHeight} }}>{
+      }</canvas>
         </div>
         <div className="brother">
           <div className="bloque">
@@ -107,37 +92,7 @@ class App extends React.Component {
   
 }
 
-class Canvas extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: 0,
-      height: 0
-    };
-  }
-  render() {
-    const { width, height } = this.props;
-    return (
-      <canvas
-        ref={(node) => (this.canvas = node)}
-        width={width}
-        height={height}
-        style={{ outline: "1px solid" }}
-      />
-    );
-  }
-  updateDimensions = () => {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
-  };
-  componentDidMount() {
-    window.addEventListener("resize", this.updateDimensions);
-    const ctx = this.canvas.getContext("2d");
-    this.props.draw(this.canvas, ctx);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions);
-  }
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
