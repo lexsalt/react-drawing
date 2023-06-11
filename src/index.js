a4c10a4b070d81fc97d27a584884d41efc9ff3c7
emilianosal <emilianosal@hotmail.com>
2023-06-11 13:19:42 +0200

    started again. Reworked counter and pointer without usestate

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 //import { UseRef } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-// const defColor = '#2196F8';
-//const tweetQuoteStyle = {background: '#1DA1F2'};
 
 class App extends React.Component {
   constructor(props) {
@@ -13,117 +11,126 @@ class App extends React.Component {
     this.state = {
       width: 1000,
       height: 100,
-      color: "white",
+      pointerColor: "black",
+      strokeSize: 4,
+      backgroundColor: "white",
       painting: true
     };
-    this.canvasClickWhite = this.canvasClickWhite(this);
-    this.canvasClickBlack = this.canvasClickBlack(this)
-    // this.canvasClickRed = this.canvasClickRed(this)
-    // this.canvasClickGreen = this.canvasClickGreen(this)
-    // this.canvasClickBlue = this.canvasClickBlue(this)
-    // this.canvasClickRandom = this.canvasClickRandom(this)    
-  }  
-   
-canvasClickWhite() {
-  console.log( "miameee")
-}
-canvasClickBlack() {
-  //this.setState({ color: "black" });
-  this.setState(state => ({ color: "black" }))
-  console.log("how deep is your love")
-}
-// canvasClickRed() {
-//   this.setState({ color: "red" });
-// }
-// canvasClickGreen() {
-//   this.setState({ color: "green" });
-// }
-// canvasClickBlue() {
-//   this.setState({ color: "blue" });
-// }
-// canvasClickRandom() {
-//   let col1 = Math.floor(Math.random() * 254) + 1;
-//     let col2 = Math.floor(Math.random() * 254) + 1;
-//     let col3 = Math.floor(Math.random() * 254) + 1;
-//     let strokeColorRandom = "rgba(" + col1 + "," + col2 + "," + col3 + ",1)";
-//     let strokeColor = strokeColorRandom;
-//   this.setState({ color: strokeColor });
-// }
-  
+    this.pointerColorBlack = this.pointerColorBlack.bind(this);
+    this.pointerColorRed = this.pointerColorRed.bind(this);
+    this.pointerColorGreen = this.pointerColorGreen.bind(this);
+    this.pointerColorBlue = this.pointerColorBlue.bind(this);
+    this.pointerColorWhite = this.pointerColorWhite.bind(this);
+    this.pointerColorRandom = this.pointerColorRandom.bind(this);
+    this.incrementStrokeSize = this.incrementStrokeSize.bind(this);
+    this.decrementStrokeSize = this.decrementStrokeSize.bind(this);
+        
+  }
+  pointerColorBlack() {
+    this.setState(state => ({ pointerColor: "black" }))
+    console.log("pointer is now color black")
+  }
+  pointerColorRed() {
+    this.setState(state => ({ pointerColor: "red" }))
+    console.log("pointer is now color Red")
+  }
+  pointerColorGreen() {
+    this.setState(state => ({ pointerColor: "green" }))
+    console.log("pointer is now color Green")
+  }
+  pointerColorBlue() {
+    this.setState(state => ({ pointerColor: "blue" }))
+    console.log("pointer is now color Blue")
+  }
+  pointerColorWhite() {
+    this.setState(state => ({ pointerColor: "white" }))
+    console.log("pointer is now color White")
+  }
+  pointerColorRandom() {
+    const newColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    this.setState(state => ({ pointerColor: newColor }))
+    console.log("pointer is now color Random")
+  }
+  incrementStrokeSize() {
+      let strokeSize = this.state.strokeSize
+            if (strokeSize < 10) {
+              this.setState(state => ({ strokeSize: strokeSize + 1 }))
+            }
+  }
+  decrementStrokeSize() {
+    let strokeSize = this.state.strokeSize
+            if (strokeSize > 1) {
+              this.setState(state => ({ strokeSize: strokeSize - 1 }))
+            }
+  }
+
   render() {
-    const canvasWidth = this.state.width
-    const canvasHeight = this.state.height
-    const backgroundColor = this.state.color
-    console.log("height: "+this.state.height)
-    console.log("width: "+this.state.width)
-    console.log("color: "+this.state.color)
+    let pointerColor = this.state.pointerColor
+    let strokeSize = this.state.strokeSize
+    //console.log("pointer es: "+pointerColor)
     return (
       <div className="parent">
-        <div id="sister" className="sister" style={{ background: backgroundColor, outline: "1px solid pink", width:{canvasWidth}, height:{canvasHeight} }}>
-        {/* <canvas>{}</canvas> */}
-        </div>
-        <div className="brother">
-          <div className="bloque">
-            <h4>Color</h4>
-            <span className="colorcito" />
-            <button id="BlackBtn">Black</button>
-            <button id="RedBtn">Red</button>
-            <button id="GreenBtn">Green</button>
-            <button id="BlueBtn">Blue</button>
-            <button id="RandomBtn">Random</button>
-            <button id="whiteBtn">White</button>
-          </div>
-          <div className="bloque">
-            <Contador></Contador>
-          </div>
-          <div className="bloque">
-            <h4>Background</h4>
-            <button id="whiteCvnBtn" onClick={this.canvasClickWhite}>Blanco</button>
-            <button id="blackCvnBtn" onClick={this.canvasClickBlack}>Black</button>
-            {/* <button id="redCvnBtn" onClick={this.canvasClickRed}>Red</button>
-            <button id="greenCvnBtn" onClick={this.canvasClickGreen}>Green</button>
-            <button id="blueCvnBtn" onClick={this.canvasClickBlue}>Blue</button>
-            <button id="randomCvnBtn" onClick={this.canvasClickRandom}>Random</button> */}
-          </div>
-        </div>
-      </div>
+               <div id="sister" className="sister" style={{ background: this.state.backgroundColor, outline: "1px solid pink" }}>
+                    <h1>Hello</h1>
+               </div>
+               <div className="brother">
+                 <div className="bloque">
+                   <h4>Color</h4>
+                   <span className="colorcito" style={{background:pointerColor}}/>
+                   <button id="BlackBtn" onClick={this.pointerColorBlack}>Black</button>
+                   <button id="RedBtn"  onClick={this.pointerColorRed}>Red</button>
+                   <button id="GreenBtn" onClick={this.pointerColorGreen}>Green</button>
+                   <button id="BlueBtn" onClick={this.pointerColorBlue}>Blue</button>
+                   <button id="whiteBtn" onClick={this.pointerColorWhite}>White</button>
+                   <button id="RandomBtn" onClick={this.pointerColorRandom}>Random</button>
+                 </div>
+                 <div className="bloque">
+                 <div>
+                   <div id="sizeLabel">Size</div>
+                   <div className="counter">
+                   <span id="strokeSize">{strokeSize}</span>
+                   </div>
+                   <button id="masSize" onClick={this.incrementStrokeSize}>+ Size</button>
+                   <button id="menosSize" onClick={this.decrementStrokeSize}>- Size</button>
+                 </div>
+                 </div>
+                 <div className="bloque">
+                   <h4>Background</h4>
+                   <button id="whiteCvnBtn">Blanco</button>
+                   <button id="blackCvnBtn">Black</button>
+       
+                 </div>
+               </div>
+             </div>
     );
   }
-  updateDimensions = () => {
-    this.setState(state => ({ width: window.innerWidth, height: window.innerHeight }))
-   }
-  componentDidMount() {
-    window.addEventListener("resize", this.updateDimensions);
-  }
-  
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions);
-  }
-}
+};
 
-function Contador() {
-  const [ count, setCount ] = useState(4)
-  function decrementCount() {
-    if (count > 0) {
-      setCount(prevCount => prevCount - 1)
-    } 
-  }
-    function incrementCount() {
-      if (count < 10) {
-        setCount(prevCount => prevCount + 1)
-      }    
-  }
-  return (
-    <div>
-            <div id="sizeLabel">Size</div>
-            <div className="counter">
-            <span id="strokeSize" >{count}</span>
-            </div>
-            <button id="masSize" onClick={incrementCount}>+ Size</button>
-            <button id="menosSize" onClick={decrementCount}>- Size</button>
-          </div>
-  )
-}
+
+// function Contador() {
+//   const [ count, setCount ] = useState(4)
+//   function decrementCount() {
+//     if (count > 0) {
+//       setCount(prevCount => prevCount - 1)
+//     } 
+//   }
+//     function incrementCount() {
+//       if (count < 10) {
+//         setCount(prevCount => prevCount + 1)
+//         console.log("1: ")
+//       }    
+//   }
+//   return (
+//     <div>
+//             <div id="sizeLabel">Size</div>
+//             <div className="counter">
+//             <span id="strokeSize" >{count}</span>
+//             </div>
+//             <button id="masSize" onClick={incrementCount}>+ Size</button>
+//             <button id="menosSize" onClick={decrementCount}>- Size</button>
+//           </div>
+//   )
+// }
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
